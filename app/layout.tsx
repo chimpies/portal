ba96adaf9e7117b674048ab851bf1470545d4469
𝗖𝗵𝗶𝗺𝗽𝗶𝗲𝘀 <gm@chimpies.xyz>
2026-08-15 02:30:17 +0700

    Update background and text colors in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
   className={`
   ${bebas.variable}
   ${inter.variable}
-  bg-[#cccecd]
-  text-black
+  bg-[#EEDECD]
+  text-[#303030]
   `}
 >
         {children}
